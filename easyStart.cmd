b5be1612ea9b02ea3bfffe62a5eed36703ab83ed
HCS <helder.costa@eduvaud.ch>
2024-03-08 14:42:31 +0100

    Ajout du dossier build pour le déploiement en production

--- easyStart.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-:: Définissez le répertoire de travail actuel
-set CWD=%CD%
-
-:: Ouvrez Windows Terminal
-wt -p "PowerShell" -d "%CWD%" powershell -NoExit -Command "code ." ; ^
-split-pane -H -p "PowerShell" -d "%CWD%" powershell -NoExit -Command npm start ; ^
-sp -V -p "PowerShell" -d "%CWD%" node ./src/srvMongoDB.js
--- src/components/tpiSchedule/Constants.jsx
+++
@@ -1,6 +0,0 @@
-// Constants.js
-export const ItemTypes = {
-    TPI_CARD: 'TpiCard', 
-    TPI_SLOT: 'TpiSlot',
-  };
-  
