3eba5a0472f2bd85aac401c3e46e26efd9c8e20c
ETML-HCS <helder.costa@eduvaud.ch>
2026-05-14 10:11:48 +0200

    release: build VS.260514

---
+++ build/assets/staticVotePublication-qveL3G8H.js
@@ -0,0 +1,3 @@
+var e=[`Régénérer le mini-site vote ?`,`Synchronise d'abord les réponses déjà déposées sur le mini-site.`,`Si le dossier distant data/ est remplacé lors de la publication, les votes web non importés peuvent être perdus.`].join(`
+
+`),t=`Avant de régénérer ou publier, synchronise les réponses web et conserve le dossier distant data/.`;export{t as n,e as t};
